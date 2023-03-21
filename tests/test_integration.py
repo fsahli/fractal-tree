bf98ecca98a870ac8b8c92c3574c59a3063c952e
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-03-21 19:20:26 +0100

    Add simple integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,48 @@
+import numpy as np
+from pathlib import Path
+import pytest
+
+
+from fractal_tree.FractalTree import Fractal_Tree_3D
+from fractal_tree.parameters import Parameters
+
+
+here = Path(__file__).absolute().parent
+
+
+@pytest.fixture
+def filename():
+    path = here / "tmp-sphere"
+    yield path.as_posix()
+    for p in here.iterdir():
+        if p.name.startswith(path.name):
+            p.unlink()
+
+
+def test_integration(filename):
+
+    param = Parameters()
+    param.meshfile = here / ".." / "examples" / "sphere.obj"
+    param.filename = filename
+
+    np.random.seed(1234)
+    branches, nodes = Fractal_Tree_3D(param)
+
+    assert len(branches) == 893
+    assert len(nodes.nodes) == 5822
+    assert np.allclose(nodes.nodes[0], [-1, 0, 0])
+    assert np.allclose(nodes.nodes[2], [-0.9990874, 0.0, 0.01975606])
+    assert branches[0].nodes == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert branches[0].triangles == [
+        3122,
+        3122,
+        3122,
+        3122,
+        3122,
+        3122,
+        3122,
+        614,
+        614,
+        614,
+    ]
+    assert branches[0].tri == 614
